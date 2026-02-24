b9484b42cf5a8dd16d8675b365e29f28884589e5
Svoboda911 <exxas.xd@gmail.com>
2026-02-24 21:16:54 +0500

    commit 7

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 #include <glad.h>
 #include <GLFW/glfw3.h>
@@ -7,14 +9,39 @@
 
 #include "shaders.h"
 
+/* 
+Нарисовать просто круг (ядро)
+
+Нарисовать движущуюся точку
+
+Заставить точку менять направление
+
+Потом добавить формулу силы
+*/
+
+struct particle {
+    glm::vec2 position;
+    glm::vec2 velocity;
+};
+
+std::vector<float> initVertices(const int& segments, const float& radius) {
+    std::vector<float> vertices = {0.0f, 0.0f, 0.0f};
+    for (int i = 0; i <= segments; i++) {
+        float angle = 2.0f * M_PI * i / segments;
+        float x = radius * cos(angle);
+        float y = radius * sin(angle);
+    }
+    
+}
+
 void program() {
     // glfw init -----------------------
-    if(!glfwInit) return;
+    if(!glfwInit()) return;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     GLFWwindow* window = glfwCreateWindow(512, 512, "project", NULL, NULL);
-    if(!window) { glfwTerminate; return;}; 
+    if(!window) {glfwTerminate(); return;}; 
     glfwMakeContextCurrent(window);
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return; 
     glViewport(0, 0, 512, 512);
@@ -22,6 +49,7 @@ void program() {
 
     // vertices data -------------------
     float vertices[] = {};
+    unsigned int indices[] = {};
     // ---------------------------------
 
     // vertices ------------------------
@@ -39,6 +67,7 @@ void program() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // ---------------------------------
 
     // shaders init --------------------
@@ -46,6 +75,8 @@ void program() {
 
     // program loop --------------------
     while(!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         shader.use();
 
@@ -60,6 +91,6 @@ void program() {
 }
 
 int main() {
-
+    program();
     return 0;
 }
