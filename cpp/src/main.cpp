bc8a834bc1c78f3ea4ccdc1c0225c3026ad17ea2
Svoboda911 <exxas.xd@gmail.com>
2026-02-24 16:07:57 +0500

    third commit

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -5,6 +5,15 @@
 
 #include <glm/glm.hpp>
 
+void program() {
+    // glfw init -----------------------
+    if(!glfwInit) return;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+}
 
 int main() {
 
